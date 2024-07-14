1c446ae84064ef3a57dde183f468c0d283e29c39
Mike Kostenko <vopros.m@gmail.com>
2024-07-14 17:00:17 +0100

    Add PUT method for updating cards in DB

--- src/main/java/eu/singledev/cashcard/CashCardController.java
+++ src/main/java/eu/singledev/cashcard/CashCardController.java
@@ -52,4 +52,15 @@ public class CashCardController {
                 .toUri();
         return ResponseEntity.created(locationOfNewCashCard).build();
     }
+
+    @PutMapping("/{requestedId}")
+    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
+        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        if (cashCard != null) {
+            CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+            cashCardRepository.save(updatedCashCard);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
